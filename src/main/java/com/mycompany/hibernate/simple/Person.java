5affedb5d966a437cdb6fa121951ea5172e67281
Ivan Putera Masli <ivanmasli@gmail.com>
2015-03-17 15:28:45 +0700

    First Commit

---
+++ src/main/java/com/mycompany/hibernate/simple/Person.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.hibernate.simple;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Person {
+    String name;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
---
+++ src/main/java/com/mycompany/hibernate/simple/Ranking.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mycompany.hibernate.simple;
+
+/**
+ *
+ * @author Ivan
+ */
+public class Ranking {
+    public Person observer;
+    public Person subject;
+    public Skill skill;
+    public Integer ranking;
+
+    public Ranking() {
+    }
+    
+}

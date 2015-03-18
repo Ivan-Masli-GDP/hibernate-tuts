adc43d972a414dd2ed0414e01aec0c1a2912084f
ivan.masli <ivan.masli@GDP-LTP-337>
2015-03-18 11:10:32 +0700

    Adding Persistence for Person

--- src/main/java/com/mycompany/hibernate/simple/persist/Person.java
+++ src/main/java/com/mycompany/hibernate/simple/persist/Person.java
@@ -3,15 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package com.mycompany.hibernate.simple.persist;
 
-package com.mycompany.hibernate.simple;
-
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  *
  * @author Ivan
  */
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
     String name;
 
     public Person() {
@@ -24,5 +34,5 @@ public class Person {
     public void setName(String name) {
         this.name = name;
     }
-    
+
 }
